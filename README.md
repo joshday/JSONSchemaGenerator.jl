adb5b2c9789d723d93f46af30db51f5f90ed5728
Josh Day <emailjoshday@gmail.com>
2023-08-28 13:02:54 -0400

    first working version

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # JSONSchemaGenerator
 
 [![Build Status](https://github.com/joshday/JSONSchemaGenerator.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/joshday/JSONSchemaGenerator.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+
+
+
+# The Goal:
+
+Create a function that inputs a `Type` and generates a JSON Schema such that:
+
+1. `JSON3.write(::MyType)` creates JSON that is valid against the schema.
+2. `JSON3.read(x, MyType)` will work when `x` is valid against the schema.
+
+
+We don't need to deal with:
--- src/JSONSchemaGenerator.jl
+++ src/JSONSchemaGenerator.jl
@@ -1,5 +1,32 @@
 module JSONSchemaGenerator
 
-# Write your package code here.
+export make_schema, roundtrip
+
+using OrderedCollections, JSON3, StructTypes
+
+
+D = OrderedDict{Symbol, Any}
+d(; kw...) = D(kw...)
+
+roundtrip(x) = JSON3.read(JSON3.write(x))
+
+gen(x) = gen(StructTypes.StructType(x), x)
+
+gen(::StructTypes.StringType, x) = d(type = "string")
+gen(::StructTypes.NumberType, x) = d(type = "number")
+gen(::StructTypes.BoolType, x) = d(type = "boolean")
+gen(::StructTypes.NullType, x) = d(type = "null")
+
+gen(::StructTypes.ArrayType, x::AbstractVector{T}) where {T} = d(type = "array", items = gen(T, x[1]))
+gen(::StructTypes.ArrayType, x::Tuple) = d(type = "array", prefixItems = gen.(x))
+
+gen(::StructTypes.DictType, x) = d(type = "object", properties = D(Symbol(k) => gen(v) for (k,v) in pairs(x)))
+
+gen(::StructTypes.SingletonType, x) = error("SingletonType not supported")
+
+# Everything else: roundtrip it to see what JSON3 does with it.
+gen(::Any, x) = gen(roundtrip(x))
+
+
 
 end
